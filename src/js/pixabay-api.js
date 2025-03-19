c7caa28b84c9c51ced734601dbe0f47e50eceebe
andrii <a.iakumiv@gmail.com>
2025-03-19 10:15:12 +0200

    Refactored the code

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,38 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
 export const fetchPhotosByQuery = query => {
-  const searchParameters = new URLSearchParams({
+  const searchParameters = {
     key: '49359014-7397220ae93b07f148b4c236e',
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-  });
+  };
 
-  return fetch(`https://pixabay.com/api/?${searchParameters}`).then(
-    response => {
-      // Error checking
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-
-      return response.json();
-    }
-  );
+  return axios.get(``, { params: searchParameters });
 };
+
+// --------------------------------------------------
+
+// export const fetchPhotosByQuery = query => {
+//   const searchParameters = new URLSearchParams({
+//     key: '49359014-7397220ae93b07f148b4c236e',
+//     q: query,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//   });
+
+//   return fetch(`https://pixabay.com/api/?${searchParameters}`).then(
+//     response => {
+//       // Error checking
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+
+//       return response.json();
+//     }
+//   );
+// };
--- src/main.js
+++ src/main.js
@@ -21,53 +21,125 @@ const hideLoader = () => {
 
 // Retrieving data from the server to create a gallery
 
-const onSearchFormSubmit = event => {
-  event.preventDefault();
+const onSearchFormSubmit = async event => {
+  try {
+    event.preventDefault();
 
-  const query = event.currentTarget.elements.search_text.value.trim();
+    const query = event.currentTarget.elements.search_text.value.trim();
 
-  if (query === '') {
-    alert('Enter text in the search field');
-    return;
+    if (query === '') {
+      alert('Enter text in the search field');
+      return;
+    }
+
+    const { data } = await fetchPhotosByQuery(query);
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        position: 'topRight',
+        message:
+          'Sorry, there are no images matching your search query. Please, try again!',
+        messageSize: '16px',
+        messageColor: '#ffffff',
+        timeout: 3000,
+        pauseOnHover: true,
+        backgroundColor: '#EF4040',
+        iconUrl: iconError,
+        layout: 2,
+      });
+
+      refs.searchForm.reset(); //clear form
+
+      refs.gallery.innerHTML = ''; //cleaning the gallery
+
+      return;
+    }
+
+    const galleryCardsTemplate = data.hits
+      .map(img => createGalleryCardMarkup(img))
+      .join('');
+
+    refs.gallery.innerHTML = galleryCardsTemplate;
+    createLightBox();
+
+    refs.searchForm.reset();
+
+    // showLoader();
+  } catch (err) {
+    console.log(err);
   }
-
-  showLoader();
-
-  fetchPhotosByQuery(query)
-    .then(({ hits }) => {
-      // We add a message that there is no such value
-      if (hits.length === 0) {
-        iziToast.error({
-          position: 'topRight',
-          message:
-            'Sorry, there are no images matching your search query. Please, try again!',
-          messageSize: '16px',
-          messageColor: '#ffffff',
-          timeout: 3000,
-          pauseOnHover: true,
-          backgroundColor: '#EF4040',
-          iconUrl: iconError,
-          layout: 2,
-        });
-
-        refs.searchForm.reset(); //clear form
-        refs.gallery.innerHTML = ''; //cleaning the gallery
-        return;
-      }
-
-      const galleryCardsTemplate = hits
-        .map(img => createGalleryCardMarkup(img))
-        .join('');
-      refs.gallery.innerHTML = galleryCardsTemplate;
-      createLightBox();
-      refs.searchForm.reset();
-    })
-    .catch(err => {
-      console.log(err);
-    })
-    .finally(() => {
-      hideLoader();
-    });
 };
 
 refs.searchForm.addEventListener('submit', onSearchFormSubmit);
+
+// ---------------------------
+
+// import { createGalleryCardMarkup, createLightBox } from './js/render-functions';
+// import { fetchPhotosByQuery } from './js/pixabay-api';
+// import iziToast from 'izitoast';
+// import iconError from './img/icon/icon-error.svg';
+
+// const refs = {
+//   searchForm: document.querySelector('.form'),
+//   gallery: document.querySelector('.gallery'),
+//   backdrop: document.querySelector('.backdrop'),
+// };
+
+// // Function to show loader
+// const showLoader = () => {
+//   refs.backdrop.classList.remove('is-hidden');
+// };
+
+// // Function to hide loader
+// const hideLoader = () => {
+//   refs.backdrop.classList.add('is-hidden');
+// };
+
+// // Retrieving data from the server to create a gallery
+
+// const onSearchFormSubmit = event => {
+//   event.preventDefault();
+//   const query = event.currentTarget.elements.search_text.value.trim();
+//   if (query === '') {
+//     alert('Enter text in the search field');
+//     return;
+//   }
+//   showLoader();
+//   fetchPhotosByQuery(query)
+//     .then(({ hits }) => {
+//       // We add a message that there is no such value
+//       if (hits.length === 0) {
+//         iziToast.error({
+//           position: 'topRight',
+//           message:
+//             'Sorry, there are no images matching your search query. Please, try again!',
+//           messageSize: '16px',
+//           messageColor: '#ffffff',
+//           timeout: 3000,
+//           pauseOnHover: true,
+//           backgroundColor: '#EF4040',
+//           iconUrl: iconError,
+//           layout: 2,
+//         });
+
+//         refs.searchForm.reset(); //clear form
+//         refs.gallery.innerHTML = ''; //cleaning the gallery
+//         return;
+//       }
+
+//       const galleryCardsTemplate = hits
+//         .map(img => createGalleryCardMarkup(img))
+//         .join('');
+//       refs.gallery.innerHTML = galleryCardsTemplate;
+//       createLightBox();
+//       refs.searchForm.reset();
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     })
+//     .finally(() => {
+//       hideLoader();
+//     });
+// };
+
+// refs.searchForm.addEventListener('submit', onSearchFormSubmit);
